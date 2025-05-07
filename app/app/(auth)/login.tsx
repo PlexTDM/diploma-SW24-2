2e11675a7ee2e12ab52f896f2457d2f848fe190e
Suldmonkh <ssuldmunh@gmail.com>
2025-05-07 14:37:12 +0800

    suldee

--- app/app/(auth)/login.tsx
+++ app/app/(auth)/login.tsx
@@ -29,6 +29,7 @@ const Login = () => {
   const signUp = () => router.push("/(auth)/signup");
 
   return (
+<<<<<<< Updated upstream
     <ThemeView>
       <View className="flex-1 bg-white dark:bg-gray-900 relative justify-center items-center pt-36">
         <BlurEllipse />
@@ -51,6 +52,76 @@ const Login = () => {
                 placeholderTextColor={theme === "dark" ? "#ccc" : "#89888E"}
               />
             </View>
+=======
+    <View className="flex-1 bg-white dark:bg-black relative justify-center items-center pt-36">
+      <BlurEllipse />
+      <View className="flex-1 bg-white rounded-3xl items-center justify-start w-96 h-96">
+        <Image
+          source={require("@/assets/img/logoLarge.png")}
+          style={{ width: "15%", height: "15%" }}
+          cachePolicy={"memory-disk"}
+          contentFit={"contain"}
+          focusable={false}
+        />
+        <Text className="text-3xl text-center mb-6 font-bold">Login</Text>
+
+        {/* Email */}
+        <View className="w-3/4 mb-4">
+          <TextInput
+            placeholder="Enter your email"
+            autoCapitalize="none"
+            className="bg-white dark:bg-gray-700 text-black dark:text-white rounded-full px-4 py-6 border border-gray-300"
+            onChangeText={setUsername}
+            placeholderTextColor={theme === "dark" ? "#ccc" : "#666"}
+          />
+        </View>
+
+        {/* Password */}
+        <View className="w-3/4 mb-1">
+          <TextInput
+            placeholder="Enter your password"
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            className="bg-white dark:bg-gray-700 text-black dark:text-white rounded-full px-4 py-6 border border-gray-300"
+            onChangeText={setPassword}
+            placeholderTextColor={theme === "dark" ? "#ccc" : "#666"}
+          />
+        </View>
+
+        {/* Forgot Password */}
+        <View className="w-3/4 items-end mb-6">
+          <TouchableOpacity onPress={() => router.push("/(auth)/ForgotPassword")}>
+            <Text className="text-blue-700 text-sm">Forgot Password?</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Login button */}
+        <View className="flex-row w-3/4 justify-center mb-4 space-x-4 mt-5">
+          <TouchableOpacity
+            onPress={logIn}
+            className="bg-black rounded-full w-full py-6 items-center"
+          >
+            <Text className="text-white font-bold">Нэвтрэх</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Divider */}
+        <View className="flex-row justify-center items-center gap-2 mt-5">
+          <Text className="bg-gray-300 w-28 h-0.5 flex justify-center items-center"></Text>
+          <Text className="text-gray-500">or login with</Text>
+          <Text className="bg-gray-300 w-28 h-0.5 flex justify-center items-center"></Text>
+        </View>
+      </View>
+
+      {/* Register link */}
+      <View className="flex-row justify-center bottom-8">
+        <Text>Don't have an account? </Text>
+        <Text className="text-blue-800" onPress={signUp}>
+          Register Now
+        </Text>
+      </View>
+>>>>>>> Stashed changes
 
             <View className="w-5/6 mb-6 relative">
               <TextInput
@@ -124,3 +195,4 @@ const Login = () => {
 };
 
 export default Login;
+
