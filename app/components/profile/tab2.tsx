3d20a7bc4efa0080ef7e01f6a2029b745ba6c378
Gereltod2 <ogee1228@gmail.com>
2025-05-12 13:00:01 +0800

    Update tab2.tsx

--- app/components/profile/tab2.tsx
+++ app/components/profile/tab2.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { useAppTheme } from "@/lib/theme";
+import { ThemeView, ThemeText } from "@/components";
+import { useLanguage } from "@/lib/language";
 
 export default function tab2() {
   return (
-    <View>
+    <ScrollView>
       <Text>tab2</Text>
-    </View>
+      
+    </ScrollView>
   );
 }
