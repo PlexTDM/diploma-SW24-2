667edb81456739ab8012cc98dc9c59d781517f37
Gereltod2 <ogee1228@gmail.com>
2025-05-13 16:47:56 +0800

    Update tab3.tsx

--- app/components/profile/tab3.tsx
+++ app/components/profile/tab3.tsx
@@ -8,7 +8,7 @@ import moment from "moment";
 
 export default function Tab3() {
   const { theme } = useAppTheme();
-  
+
   // Dynamic names
   const yourName = "You";
   const opponentName = "Alex";
@@ -28,7 +28,6 @@ export default function Tab3() {
   return (
     <ScrollView className="p-4">
       <View className="bg-white dark:bg-gray-800 rounded-2xl p-4 shadow-sm mt-8">
-
         {/* Top avatars and dynamic title */}
         <View className="flex-row justify-evenly items-center border-b pb-4 border-gray-300 dark:border-gray-600">
           <Image
@@ -36,9 +35,15 @@ export default function Tab3() {
             style={{ width: 53, height: 50 }}
           />
           <View className="items-center">
-            <ThemeText className="text-xl font-bold">
-              {yourName} vs {opponentName}
-            </ThemeText>
+            <View className="flex-row jsutify-center items-center gap-4">
+              <Text className="text-xl font-bold text-[#E9509F]">
+                {yourName}
+              </Text>
+              <ThemeText>vs</ThemeText>
+              <Text className="text-xl font-bold text-blue-500">
+                {opponentName}
+              </Text>
+            </View>
             <ThemeText className="text-sm text-gray-500">3 workouts</ThemeText>
           </View>
           <Image
@@ -54,15 +59,23 @@ export default function Tab3() {
 
         {/* Steps */}
         <View className="flex-row justify-center space-x-1">
-          <ThemeText className="font-bold text-xl text-[#E9509F]">{yourSteps}</ThemeText>
+          <ThemeText className="font-bold text-xl text-[#E9509F]">
+            {yourSteps}
+          </ThemeText>
           <ThemeText className="text-lg text-gray-500 mx-2">vs</ThemeText>
-          <Text className="font-bold text-xl text-blue-500 dark:text-blue-400">{alexSteps}</Text>
+          <Text className="font-bold text-xl text-blue-500">
+            {alexSteps}
+          </Text>
         </View>
 
         {/* Progress Bars */}
         <View className="flex-row items-center">
           <View className="mr-4 bg-gray-200 dark:bg-gray-700 p-3 rounded-full">
-            <Icon source="run-fast" size={36} color={theme === "dark" ? "#fff" : "#000"} />
+            <Icon
+              source="run-fast"
+              size={36}
+              color={theme === "dark" ? "#fff" : "#000"}
+            />
           </View>
 
           <View className="flex-1">
