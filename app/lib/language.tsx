08fcae9a02bfcb442a918a3a73e0544b6a578a79
miurose <rosamnjk@gmail.com>
2025-05-15 15:00:12 +0800

    cookie up

--- app/lib/language.tsx
+++ app/lib/language.tsx
@@ -1,4 +1,4 @@
-import Calories from "@/app/home/calories";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createContext, useContext, useEffect, useState } from "react";
 
@@ -170,9 +170,14 @@ export const mn = {
     timeline:"Түүх",
     stats:"Статистик",
     duels:"Өрсөлдөөн",
+  } ,
+  fortune:{
+    title:"",
+    desc:"Таны зорилгоо тодорхойлно",
   }
 };
 
+ 
 export const en = {
 
    question1: {
