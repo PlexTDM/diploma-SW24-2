8c21a68710e246bf322dae87aa50e6a8cb52a34d
PlexTDM <tengisteka0631@gmail.com>
2025-05-21 03:25:10 +0800

    lint fix

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { request, Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
@@ -442,34 +442,11 @@ class AuthController {
   }
 
   public static async callback(req: Request, res: Response): Promise<any> {
-    // export async function GET(request: Request) {
-    //   const incomingParams = new URLSearchParams(request.url.split("?")[1]);
-    //   const combinedPlatformAndState = incomingParams.get("state");
-    //   if (!combinedPlatformAndState) {
-    //     return Response.json({ error: "Invalid state" }, { status: 400 });
-    //   }
-    //   // strip platform to return state as it was set on the client
-    //   const platform = combinedPlatformAndState.split("|")[0];
-    //   const state = combinedPlatformAndState.split("|")[1];
-
-    //   const outgoingParams = new URLSearchParams({
-    //     code: incomingParams.get("code")?.toString() || "",
-    //     state,
-    //   });
-
-    //   return Response.redirect(
-    //     (platform === "web" ? BASE_URL : APP_SCHEME) +
-    //       "?" +
-    //       outgoingParams.toString()
-    //   );
-    // }
-
     const incomingParams = new URLSearchParams(req.url.split("?")[1]);
     const combinedPlatformAndState = incomingParams.get("state");
     if (!combinedPlatformAndState) {
       return res.status(400).json({ error: "Invalid state" });
     }
-    const platform = combinedPlatformAndState.split("|")[0];
     const state = combinedPlatformAndState.split("|")[1];
 
     const outgoingParams = new URLSearchParams({
