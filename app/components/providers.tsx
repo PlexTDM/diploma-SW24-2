1d441bbcba8871b23aadf11565e0d606ba1dc9ef
unnamed <tengisteka0631@gmail.com>
2025-05-21 12:18:25 +0800

    shit

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -4,16 +4,14 @@ import React from "react";
 import { View } from "react-native";
 import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 import { AuthProvider } from "@/context/auth";
-import { EdgeStoreProvider } from "@/lib/edgestore";
+
 const ThemeUser = ({ children }: { children: React.ReactNode }) => {
   const { theme } = useAppTheme();
   return (
     <View className={`${theme} flex-1`}>
       <PaperProvider theme={theme === "dark" ? MD3DarkTheme : MD3LightTheme}>
         <AuthProvider>
-          <EdgeStoreProvider>
-            <LanguageProvider>{children}</LanguageProvider>
-          </EdgeStoreProvider>
+          <LanguageProvider>{children}</LanguageProvider>
         </AuthProvider>
       </PaperProvider>
     </View>
