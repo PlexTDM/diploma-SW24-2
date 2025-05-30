2a9b65c96e6ad619656cfb53794749ff1d808874
PlexTDM <tengisteka0631@gmail.com>
2025-05-30 22:18:55 +0800

    merge language + streak

--- server/src/middleware/rateLimit.ts
+++ server/src/middleware/rateLimit.ts
@@ -1,10 +1,10 @@
-import rateLimit from "express-rate-limit"
+import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-    message: { error: "Too many requests, please try again later." },
-    headers: true, // Include rate limit headers in response
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 150, // Limit each IP to 100 requests per windowMs
+  message: { error: "Too many requests, please try again later." },
+  headers: true, // Include rate limit headers in response
+});
 
-export default limiter
+export default limiter;
